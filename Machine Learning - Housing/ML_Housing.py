97f5a5b866c9cf397493a650012796a82455929d
Jaroslaw <94965143+kociec@users.noreply.github.com>
2021-11-30 20:05:28 +0100

    Add files via upload

--- Machine Learning - Housing/ML_Housing.py
+++ Machine Learning - Housing/ML_Housing.py
@@ -6,7 +6,6 @@ import pandas as pd
 
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import r2_score
 
 from sklearn.linear_model import LinearRegression
 from sklearn.linear_model import Ridge
