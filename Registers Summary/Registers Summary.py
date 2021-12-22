64cd20796c5b1f1f98e85666edd524ec7dd9e9ae
Jaroslaw <94965143+kociec@users.noreply.github.com>
2021-12-22 15:13:55 +0100

    Add files via upload

--- Registers Summary/Registers Summary.py
+++ Registers Summary/Registers Summary.py
@@ -2,6 +2,8 @@
 import re
 import pandas as pd
 import time
+import warnings
+warnings.filterwarnings('ignore')
 
 startTime = time.time()                # start pomiaru czasu wykonania skryptu
 inWork = True                          # wybór scieżek: True - praca, False - dom
@@ -11,9 +13,10 @@ print('   >>>>> OBLICZENIA W TOKU...  <<<<<')
 # wczytanie rejestru przed scaleniem
 
 if inWork == True:
-    path =  r'.\przed_scaleniem_raw.txt'
+    path =  r'.\przed_scaleniem.txt'
+
 else:
-    path =  r'F:\DataScience\Data\rejestr\przed_scaleniem_raw.txt'  
+    path =  r'F:\DataScience\Data\rejestr\przed_scaleniem.txt'  
 
 file = open(path, "r")                # otwarcie pliku przed_scaleniem_raw.txt
 
@@ -30,7 +33,7 @@ for a in file.readlines():            # przejcie linijka po linijce przez plik
     if a[4].endswith('50'):           # zmuszenie Pythona do poprawnego zaokrąglania '50tki' np. 1.7750 do 1.78 a nie 1.77
         a[4] = a[4] + '1'
     
-    numery_przed.append(a[1])         # rozpakowanie danych z listy 'a' i wpisanie do odpowiednich list z linijek 19-22
+    numery_przed.append(a[1])         # rozpakowanie danych z listy 'a' i wpisanie do odpowiednich list z linijek 23-26
     pow_przed.append(round(float(a[3]), 4))
     pkt_przed.append(round(float(a[4]), 2))
     jedn_przed.append(int(a[0]))
@@ -51,6 +54,9 @@ pkts = []
 nrs = []
 jedns = []
 
+nr_temp_1 = []                       # zadeklarowanie list zbierających dane tymczasowe potrzebne do sortowania dwupoziomowego
+nr_temp_2 = []
+
 for i in range(len(unique)):
     
     nr_ok = unique[i]                                                    # odwołanie do kolejnych numerów działek z listy unique
@@ -61,24 +67,49 @@ for i in range(len(unique)):
     
     temp.reset_index(inplace = True)                                     # zresetowanie indeksu, by móc odwołać sie do pierwszego elementu linijkę niżej
     jedn_ok = temp['jedn_przed'][0] 
-    
-    jedns.append(jedn_ok)                                                # wypełnienie list (z linijek 49-52) zbierających dane ostateczne
+
+    if '/' not in nr_ok:                                                 # pętla generująca dane do sortwania dwupoziomowego
+        
+        nr_1 = int(nr_ok)                                                # jesli numer bez '/' to weź go, a numer łamany wpisz jako 0
+        nr_2 = 0
+        
+    else:
+        
+        nr_oks = nr_ok.split('/')                                        # jesli numer z '/', to rozdziel go na 2 częsci
+        nr_1 = int(nr_oks[0])
+        nr_2 = int(nr_oks[1])      
+   
+    jedns.append(jedn_ok)                                                # wypełnienie list (z linijek 52-55) zbierających dane ostateczne
     pows.append(pow_ok) 
     pkts.append(pkt_ok)
     nrs.append(nr_ok)
+    
+    nr_temp_1.append(nr_1)                                               # wypełnienie list (z linijek 57-58) zbierających dane tymczasowe
+    nr_temp_2.append(nr_2)
 
 # utworzenie ostatecznego DataFrame z rejestru przed scaleniem na postawie powyższych 4 list i ustawienie nazw kolumn
 
-data_przed = pd.DataFrame(data = [jedns, nrs, pows, pkts]).transpose()
-data_przed.columns = ['jedn_przed','nr_przed','pow_przed','pkt_przed']
+data_przed = pd.DataFrame(data = [jedns, nrs, nr_temp_1, nr_temp_2, pows, pkts]).transpose()
+data_przed.columns = ['jedn_przed','nr_przed', 'temp_1', 'temp_2', 'pow_przed','pkt_przed']
+
+# sortowanie dwupoziomowe danych dzięki tymczasowym kolumnom
+
+data_przed = data_przed.sort_values(by = ['jedn_przed','temp_1', 'temp_2'], ignore_index = True)
+
+# usunięcie kolumn tymczasowych
+
+del data_przed['temp_1']
+del data_przed['temp_2']
+
 
 ###############################################################################
 # wczytanie rejestru po scaleniu
 
 if inWork == True:
-    path =  r'.\po_scaleniu_raw.txt'
+    path =  r'.\po_scaleniu.txt'
+
 else:
-    path =  r'F:\DataScience\Data\rejestr\po_scaleniu_raw.txt' 
+    path =  r'F:\DataScience\Data\rejestr\po_scaleniu.txt' 
 
 file = open(path, "r")             # otwarcie pliku po_scaleniu_raw.txt
 
@@ -95,7 +126,7 @@ for a in file.readlines():         # przejcie linijka po linijce przez plik
     if a[4].endswith('50'):        # zmuszenie Pythona do poprawnego zaokrąglania '50tki' np. 1.7750 do 1.78 a nie 1.77
         a[4] = a[4] + '1'    
 
-    numery_po.append(a[1])         # rozpakowanie danych z listy 'a' i wpisanie do odpowiednich list z linijek 85-88
+    numery_po.append(a[1])         # rozpakowanie danych z listy 'a' i wpisanie do odpowiednich list z linijek 116-119
     pow_po.append(round(float(a[3]), 4))   
     pkt_po.append(round(float(a[4]), 2))
     jedn_po.append(int(a[0]))
@@ -114,6 +145,9 @@ pkts = []
 nrs = []
 jedns = []
 
+nr_temp_1 = []                                                       # zadeklarowanie list zbierających dane tymczasowe potrzebne do sortowania dwupoziomowego
+nr_temp_2 = []
+
 for i in range(len(unique)):
     
     nr_ok = unique[i]                                                 # odwołanie do kolejnych numerów działek z listy unique
@@ -125,15 +159,38 @@ for i in range(len(unique)):
     temp.reset_index(inplace = True)                                  # zresetowanie indeksu, by móc odwołać sie do pierwszego elementu linijkę niżej
     jedn_ok = temp['jedn_po'][0] 
     
-    jedns.append(jedn_ok)                                             # wypełnienie list (z linijek 111-114) zbierających dane ostateczne
+    if '/' not in nr_ok:                                              # pętla generująca dane do sortwania dwupoziomowego
+        
+        nr_1 = int(nr_ok)                                             # jesli numer bez '/' to weź go, a numer łamany wpisz jako 0
+        nr_2 = 0
+        
+    else:
+        
+        nr_oks = nr_ok.split('/')                                     # jesli numer z '/', to rozdziel go na 2 częsci
+        nr_1 = int(nr_oks[0])
+        nr_2 = int(nr_oks[1])        
+    
+    jedns.append(jedn_ok)                                             # wypełnienie list (z linijek 143-146) zbierających dane ostateczne
     pows.append(pow_ok) 
     pkts.append(pkt_ok)
     nrs.append(nr_ok)
+    
+    nr_temp_1.append(nr_1)                                            # wypełnienie list (z linijek 148-149) zbierających dane ostateczne
+    nr_temp_2.append(nr_2)
 
 # utworzenie ostatecznego DataFrame z rejestru po scaleniu na postawie powyższych 4 list i ustawienie nazw kolumn
 
-data_po = pd.DataFrame(data = [jedns, nrs, pows, pkts]).transpose()
-data_po.columns = ['jedn_po','nr_po','pow_po','pkt_po']
+data_po = pd.DataFrame(data = [jedns, nrs, nr_temp_1, nr_temp_2, pows, pkts]).transpose()
+data_po.columns = ['jedn_po','nr_po', 'temp_1', 'temp_2', 'pow_po','pkt_po']
+
+# sortowanie dwupoziomowe danych dzięki tymczasowym kolumnom
+
+data_po = data_po.sort_values(by = ['jedn_po','temp_1', 'temp_2'], ignore_index = True)
+
+# usunięcie kolumn tymczasowych
+
+del data_po['temp_1']
+del data_po['temp_2']
 
 ###############################################################################
 # zapis danych do pliku wynikowego OUTPUT.txt
@@ -143,6 +200,7 @@ max_jedn = max(data_przed['jedn_przed'].max(), data_po['jedn_po'].max())   # wyz
 
 if inWork == True:
     path =  r'.\OUTPUT.txt'
+
 else:
     path =  r'F:\DataScience\Data\rejestr\OUTPUT.txt'
 
@@ -160,13 +218,15 @@ for i in range(1, max_jedn + 1):                                           # prz
     przed_roboczy = concated_roboczy[['jedn_przed', 'nr_przed', 'pkt_przed', 'pow_przed']]       # wydzielenie danych przed scaleniem
     
     po_roboczy = concated_roboczy[['jedn_po', 'nr_po', 'pkt_po', 'pow_po']]                      # wydzielenie danych po scaleniu
+    po_roboczy.dropna(inplace = True)                                                            # usunięcie pustych wierszy z NaN
     
-    po_roboczy = po_roboczy.sort_values(by = 'nr_po', ignore_index = True)                       # posortowanie nr działek oraz reset indeksu 
+    po_roboczy.reset_index(inplace = True)                                                       # reset indexu do postaci 0-n
+    del po_roboczy['index']                                                                      # usunięcie zbędnej kolumny ze starym indeksem
     po_roboczy.dropna(inplace = True)                                                            # usunięcie pustych wierszy z NaN
     
     joined = przed_roboczy.join(po_roboczy)                                # ostateczne złączenie danych przed i po scaleniu
     joined.dropna(inplace = True, how = 'all')                             # usunięcie ewentualnych pustych wierszy
-    joined.fillna(value = int(0), inplace = True)                          # wypełnienie wartosci NaN zerami (wartosc numeryczna i zawsze się zsumuje z resztą)
+    joined.fillna(value = 0, inplace = True)                          # wypełnienie wartosci NaN zerami (wartosc numeryczna i zawsze się zsumuje z resztą)
     
     if len(joined) == 0:                # jesli pusty DataFrame (nie było takiego nr jedn. rej. przed i po scaleniu - to nie rób nic i idź dalej)
         pass
@@ -187,7 +247,7 @@ for i in range(1, max_jedn + 1):                                           # prz
                 powierzchnia_po = joined['pow_po'][j]
                 punkt_po = joined['pkt_po'][j]
                 
-                pattern = "{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\n"      # utworzenie wzorca do zapisu i zapis pierwszej linijki z i-tej jednostki do pliku
+                pattern = "{}\t{}\t{}\t{:.2f}\t{:.4f}\t{}\t{}\t{:.2f}\t{:.4f}\n"      # utworzenie wzorca do zapisu i zapis pierwszej linijki z i-tej jednostki do pliku
                 line = pattern.format(lp,jednostka_przed, numer_przed, punkt_przed, powierzchnia_przed, jednostka_po, numer_po, punkt_po, powierzchnia_po)
                 f.write(line)
                 
@@ -203,7 +263,7 @@ for i in range(1, max_jedn + 1):                                           # prz
                 powierzchnia_po = joined['pow_po'][j]
                 punkt_po = joined['pkt_po'][j]
                 
-                pattern = "{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\n"      # utworzenie wzorca do zapisu i zapis kolejnych linijek z i-tej jednostki do pliku
+                pattern = "{}\t{}\t{}\t{:.2f}\t{:.4f}\t{}\t{}\t{:.2f}\t{:.4f}\n"      # utworzenie wzorca do zapisu i zapis kolejnych linijek z i-tej jednostki do pliku
                 line = pattern.format('','', numer_przed, punkt_przed, powierzchnia_przed, '', numer_po, punkt_po, powierzchnia_po)
                 f.write(line)
                     
@@ -213,16 +273,23 @@ for i in range(1, max_jedn + 1):                                           # prz
         sum_pkt_po = round(joined['pkt_po'].sum(), 2)                # obliczenie sumy punktów i powierzchni po scaleniu
         sum_pow_po = round(joined['pow_po'].sum(), 4)
         
-        pattern = "{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\n"             # utworzenie wzorca do zapisu i zapis podsumowania i-tej jednostki do pliku
+        pattern = "{}\t{}\t{}\t{:.2f}\t{:.4f}\t{}\t{}\t{:.2f}\t{:.4f}\n"             # utworzenie wzorca do zapisu i zapis podsumowania i-tej jednostki do pliku
         line = pattern.format('', '', 'Razem:', sum_pkt_przed, sum_pow_przed, '','Razem:', sum_pkt_po, sum_pow_po)
         f.write(line)
         
-        lp += 1                                                      # inkrementacja liczby porządkowej Lp po skończonym zapisie danej jednostki
+        lp += 1                                                      # inkrementacja liczby porzadkowej Lp po skończonym zapisie danej jednostki
                 
 f.close()
 
 stopTime = time.time()                                               # zakończenie pomiaru czasu i wydruk wyniku
 print('SCRIPT EXECUTED IN:', round(stopTime - startTime, 2), 's')
+
+
+
+
+
+
+
 
 
 
